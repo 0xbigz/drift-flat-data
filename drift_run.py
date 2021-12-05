7cc8c755e1a78ab1ea8631fa31c7ddd87a8db16a
0xbigz <83473873+0xbigz@users.noreply.github.com>
2021-12-04 20:23:11 -0500

    add drift_run.py

---
+++ drift_run.py
@@ -0,0 +1,40 @@
+import asyncio
+import pandas as pd
+import os
+import json
+
+import sys
+sys.path.append("drift-py/")
+
+
+from drift.drift import Drift, load_config, MARKET_INDEX_TO_PERP
+
+
+def drift_py():
+    USER_AUTHORITY = ""
+    drift = Drift(USER_AUTHORITY)
+    asyncio.run(drift.load())
+    return drift
+
+
+def drift_history_df(drift):
+    history_df = asyncio.run(drift.load_history_df())
+    return history_df
+
+
+if __name__ == '__main__':
+    drift = drift_py()
+    df_dict = drift_history_df(drift)
+    os.makedirs('data/', exist_ok=True)
+
+    with open('data/config.json','w') as f:
+        json.dump({'last_update': str(drift.last_update)}, f)
+
+    markets = drift.market_summary()
+    markets.to_csv("data/markets_state.csv")
+
+    users = drift.user_summary()
+    users.to_csv("data/users_state.csv")    
+
+    for key, df in df_dict.items():
+        df.to_csv('data/%s_history.csv' % str(key))
